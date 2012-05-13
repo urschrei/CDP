e5df62c01909986bb6e8dcb2544944a66680febf
Stephan Hügel <urschrei@gmail.com>
2012-05-14 00:15:56 +0100

    Use alembic commands natively, instead of shelling out

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 from fabric.api import env, run, local, require, cd, put
 from fabric.decorators import task
 from fabric.utils import abort
@@ -5,11 +9,16 @@ from fabric.contrib.console import confirm
 from fabric.context_managers import settings, hide, prefix
 from fabric.colors import cyan, yellow, green, red
 from time import time
+from alembic.config import Config
+from alembic import command
 
 import sys
 import os
 
+
 env.basename = os.path.dirname(__file__)
+alembic_cfg = Config(os.path.join(os.path.dirname(env.basename), "alembic.ini"))
+
 
 @task
 def run_app():
@@ -34,8 +43,7 @@ def upgrade_db(rev="head"):
     Upgrade DB to specified revision or head. Dev only
     """
     print(cyan("Running Alembic migrations, upgrading DB to %s" % rev))
-    with cd(env.basename):
-        local("venv/bin/alembic upgrade %s" % rev)
+    command.upgrade(alembic_cfg, rev)
 
 
 @task
@@ -44,5 +52,4 @@ def downgrade_db(rev="base"):
     Downgrade DB to specified revision or base. Dev only
     """
     print(cyan("Running Alembic migrations, downgrading DB to %s" % rev))
-    with cd(env.basename):
-        local("venv/bin/alembic downgrade %s" % rev)
+    command.downgrade(alembic_cfg, rev)

f294068b52f2cd10b9146f61f57c5bc183bb85f0
Stephan Hügel <urschrei@gmail.com>
2012-05-15 23:14:05 +0100

    Frontend fixes
    Separately bundle Bootstrap and Bootstrap-responsive
    Bundle the Bootstrap static nav CSS
    Bundle local html5shim version
    
    Update templates with new responsive layout and static nav

--- app/__init__.py
+++ app/__init__.py
@@ -26,20 +26,39 @@ manifest_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '.stati
 assets.manifest = 'file://%s' % manifest_path
 
 js = Bundle(
-    'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+    'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js',
     'js/bootstrap.js',
+    'js/bootstrap-transition.js',
     'js/jquery.easing.1.3.js',
     filters='yui_js',
     output='gen/packed.%(version)s.js')
 assets.register('js_all', js)
-css = Bundle(
+
+shim = Bundle(
+        'js/html5shiv.js',
+        filters='yui_js',
+        output='gen/html5shim.%(version)s.js')
+assets.register('html5shim', shim)
+
+# bootstrap and responsive have to be bundled separately, or they don't work
+bootstrap = Bundle(
         'http://yui.yahooapis.com/3.5.1/build/cssreset/cssreset-min.css',
         'css/bootstrap.css',
+        filters='yui_css',
+        output='gen/bootstrap.%(version)s.css')
+assets.register('bootstrap', bootstrap)
+
+bootstrap_responsive = Bundle(
         'css/bootstrap-responsive.css',
         filters='yui_css',
-        output='gen/packed.%(version)s.css')
-assets.register('css_all', css)
+        output='gen/bootstrap_responsive.%(version)s.css')
+assets.register('bootstrap_responsive', bootstrap_responsive)
 
+static_nav = Bundle(
+        'css/static-nav.css',
+        filters='yui_css',
+        output='gen/static_nav.%(version)s.css')
+assets.register('static_nav', static_nav)
 # import our own blueprints here if necessary
 # from apps.foo.views import foo_app
 # attach any blueprints

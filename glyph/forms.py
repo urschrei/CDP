3d688443ae9ba99ddb6d8169b4cbd4b1a1003a3d
Stephan Hügel <urschrei@gmail.com>
2012-05-19 22:52:06 +0100

    Tidying up form

--- glyph/forms.py
+++ glyph/forms.py
@@ -4,6 +4,6 @@ from flaskext.wtf import Form, Required, TextAreaField, RadioField, HiddenField,
 class RecordForm(Form):
     record = TextAreaField(u"Record Text",
     [Required(
-        u"Record Text"),
+        u"Please enter some text."),
         validators.length(max=50,
             message=u"Please use 50 characters or less")])

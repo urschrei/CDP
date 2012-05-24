65ce01d64057842483b0d0cf78f3642133141d2c
Stephan Hügel <urschrei@gmail.com>
2012-05-24 10:48:14 +0100

    Adding method table

---
+++ db/versions/106748f03f40_create_method_table.py
@@ -0,0 +1,27 @@
+"""create method table
+
+Revision ID: 106748f03f40
+Revises: 14b7af67432b
+Create Date: 2012-05-24 10:45:57.882092
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '106748f03f40'
+down_revision = '14b7af67432b'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'method',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(50), nullable=False),
+    )
+    op.create_index('idx_name', 'method', ['name'])
+
+
+def downgrade():
+    sa.drop_table('method')

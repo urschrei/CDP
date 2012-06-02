ec0aaea258e727d0a5a6667f317c0b6d761cafdc
Stephan Hügel <urschrei@gmail.com>
2012-06-02 13:55:57 +0100

    Allow filtering by ruler in tablet view

--- glyph/views.py
+++ glyph/views.py
@@ -36,19 +36,27 @@ def tablets():
     Show result of restricting records using various criteria
     """
     q = Tablet.query
+    if request.args.get("ruler"):
+        q = q.join(ruler_tablet).filter(
+            Ruler.name == request.args.get("ruler"))
     if request.args.get("medium"):
-        q = q.join(Medium).filter(Medium.name == request.args.get("medium"))
+        q = q.join(Medium).filter(
+            Medium.name == request.args.get("medium"))
     if request.args.get("script_type"):
-        q = q.join(Script_Type)\
-            .filter(Script_Type.script == request.args.get("script_type"))
+        q = q.join(Script_Type).filter(
+            Script_Type.script == request.args.get("script_type"))
     if request.args.get("city"):
-        q = q.join(City, Tablet.city_id == City.id)\
-            .filter(City.name == request.args.get("city"))
+        q = q.join(City, Tablet.city_id == City.id).filter(
+            City.name == request.args.get("city"))
     if request.args.get("locality"):
-        q = q.join(City, Tablet.city_id == City.id).join(Locality)\
-            .filter(Locality.area == request.args.get("locality"))
+        q = q.join(Locality).filter(
+            Locality.area == request.args.get("locality"))
     if request.args.get("period"):
-        q = q.join(Period).filter(Period.name == request.args.get("period"))
+        q = q.join(Period).filter(
+            Period.name == request.args.get("period"))
+    if request.args.get("sub_period"):
+        q = q.join(Sub_Period).filter(
+            Sub_Period.name == request.args.get("sub_period"))
     if request.args.get("sent_from"):
         q = q.join(Correspondent, Tablet.from_id == Correspondent.id)\
             .filter(Correspondent.name == request.args.get("sent_from"))
@@ -56,22 +64,29 @@ def tablets():
         q = q.join(Correspondent, Tablet.to_id == Correspondent.id)\
             .filter(Correspondent.name == request.args.get("sent_to"))
     if request.args.get("year"):
-        q = q.join(Year).filter(Year.year == request.args.get("year"))
+        q = q.join(Year).filter(
+            Year.year == request.args.get("year"))
     if request.args.get("eponym"):
-        q = q.join(Year).join(Eponym)\
-            .filter(Eponym.name == request.args.get("eponym"))
+        q = q.join(Year).join(Eponym).filter(
+            Eponym.name == request.args.get("eponym"))
     if request.args.get("text_vehicle"):
-        q = q.join(Text_Vehicle).filter(Text_Vehicle.name == request.args.get("text_vehicle"))
+        q = q.join(Text_Vehicle).filter(
+            Text_Vehicle.name == request.args.get("text_vehicle"))
     if request.args.get("language"):
-        q = q.join(Language).filter(Language.name == request.args.get("language"))
+        q = q.join(Language).filter(
+            Language.name == request.args.get("language"))
     if request.args.get("method"):
-        q = q.join(Method).filter(Method.name == request.args.get("method"))
+        q = q.join(Method).filter(
+            Method.name == request.args.get("method"))
     if request.args.get("genre"):
-        q = q.join(Genre).filter(Genre.name == request.args.get("genre"))
+        q = q.join(Genre).filter(
+            Genre.name == request.args.get("genre"))
     if request.args.get("function"):
-        q = q.join(Function).filter(Function.name == request.args.get("function"))
+        q = q.join(Function).filter(
+            Function.name == request.args.get("function"))
     if request.args.get("dynasty"):
-        q = q.join(Dynasty).filter(Dynasty.name == request.args.get("dynasty"))
+        q = q.join(Dynasty).filter(
+            Dynasty.name == request.args.get("dynasty"))
     page = q.paginate(1, per_page=20)
     return render_template('tablets.jinja', page=page)
 

47d703e01a7f04f40876c6c284f9b9d479ba9022
Stephan Hügel <urschrei@gmail.com>
2012-06-10 19:21:37 +0100

    Generate a proper secret key for WTForms CSRF

--- glyph/config/common.py
+++ glyph/config/common.py
@@ -4,4 +4,5 @@ DEBUG = False
 # currently the same for all environments
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:@127.0.0.1/glyph?charset=utf8'
 
-SECRET_KEY = 'Rameses'
+# os.urandom(128).encode('hex') should do the trick
+SECRET_KEY = 'ce975af04b05782e6de27639bb0a64c9580efd20e03ed900c8d5014b35a9cdad161d5222533e7b63098371bbfb2f99a9b85c7a4b9fbff1662ba67069f743217376cfc4bdfbbd0109d06d63684cbf4d1cf237a02313e6d43887fe74635001da532fdd7f0b4372e9e828556b689819513673cda3cee6a5402a0da6fcb14d3a827f'

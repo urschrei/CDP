42d3a73c16079c420ae84ac89fed17ba955c2441
Stephan Hügel <urschrei@gmail.com>
2013-05-15 21:09:25 +0100

    Tidy up route arg lists

--- apps/glyph/views.py
+++ apps/glyph/views.py
@@ -21,7 +21,9 @@ def index():
     return render_template('index.jinja', form=form)
 
 
-@glyph.route('/tablet/<int:tablet_id>')
+@glyph.route(
+    '/tablet/<int:tablet_id>',
+    methods=['GET'])
 def tablet(tablet_id):
     tablet = Tablet.query.get_or_404(tablet_id)
     # split Signs into 12-item chunks
@@ -30,8 +32,13 @@ def tablet(tablet_id):
     return render_template('tablet.jinja', tablet=tablet, chunks=chunked)
 
 
-@glyph.route('/tablets', methods=['GET'], defaults={'page': 1})
-@glyph.route('/tablets/<int:page>', methods=['GET'])
+@glyph.route(
+    '/tablets',
+    methods=['GET'],
+    defaults={'page': 1})
+@glyph.route(
+    '/tablets/<int:page>',
+    methods=['GET'])
 def tablets(page):
     """
     Show result of restricting records using various criteria

810b4c8b5c786820c03b287d4d6c36ad5ad77086
Stephan Hügel <urschrei@gmail.com>
2013-05-17 02:06:55 +0100

    Adding compiled utcnow server default

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -1,7 +1,28 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.sql import expression
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.types import DateTime
 
 db = SQLAlchemy()
+
+
+class utcnow(expression.FunctionElement):
+    """ UTC Timestamp for compilation """
+    type = DateTime()
+
+
+# Define DB-specific utcnow() functions
+@compiles(utcnow, 'postgresql')
+def pg_utcnow(element, compiler, **kw):
+    """ Postgres UTC Timestamp """
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
+
+
+@compiles(utcnow, 'mysql')
+def my_utcnow(element, compiler, **kw):
+    """ MySQL UTC Timestamp """
+    return "UTC_TIMESTAMP()"
 
 
 class GlyphMixin(object):
@@ -17,3 +38,4 @@ class GlyphMixin(object):
     __mapper_args__ = {'always_refresh': True}
 
     id = db.Column(db.Integer, primary_key=True)
+    # timestamp = db.Column(DateTime, nullable=False, server_default=utcnow())

e38359ce23a22d3ff4aace49283ddb0ee17911b8
Stephan Hügel <urschrei@gmail.com>
2014-08-16 12:58:56 +0100

    Fix naming conventions
    By 'fix', I mean 'inject the dict into the MetaData() instance directly'
    
    Hahaha Python!

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -5,15 +5,17 @@ from sqlalchemy.sql import expression
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.types import DateTime
 
-db = SQLAlchemy()
-
-meta = MetaData(naming_convention={
+naming_convention = {
         "ix": 'ix_%(column_0_label)s',
         "uq": "uq_%(table_name)s_%(column_0_name)s",
         "ck": "ck_%(table_name)s_%(constraint_name)s",
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
         "pk": "pk_%(table_name)s"
-      })
+      }
+
+db = SQLAlchemy()
+db.metadata.naming_convention = naming_convention
+
 
 class utcnow(expression.FunctionElement):
     """ UTC Timestamp for compilation """

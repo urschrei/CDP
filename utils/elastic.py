43d6c26757de9c70aff1b6d5b1d1ca39b7325a36
Stephan Hügel <urschrei@gmail.com>
2014-09-03 23:57:57 +0100

    Adding basic ElasticSearch load & query example

---
+++ utils/elastic.py
@@ -0,0 +1,31 @@
+# brew install ElasticSearch
+# ensure you've installed a JDK
+# launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
+# launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
+# install ElasticSearch Head
+# plugin -install mobz/elasticsearch-head
+# available at http://localhost:9200/_plugin/head/
+
+from pyelasticsearch import ElasticSearch
+es = ElasticSearch('http://localhost:9200/')
+
+res = db.session.query(Sign).all()
+for r in res:
+    d = r.__dict__
+    d.pop('_sa_instance_state', None)
+
+# bulk-index the cleaned results
+es.bulk_index('cdpp', 'sign', res, id_field='id')
+
+# construct a fuzzy search query over the signs
+q = {
+    "query": {
+        "fuzzy_like_this_field" : {
+            "sign.sign_ref" : {
+                "like_text" : "GESU",
+                "max_query_terms" : 10
+            }
+        },
+    }
+}
+es.search(qt, index='cdpp')
